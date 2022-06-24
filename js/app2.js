fa9388c3639a0ffc6edc0e5dedc3fefb5284f257
julidas <juliandascenzo@gmail.com>
2022-06-24 00:33:38 -0300

    destructuring

--- js/app2.js
+++ js/app2.js
@@ -5,17 +5,18 @@ const cardcontainer = document.getElementById('cardcontainer')
 
 
 arrayProducto.forEach((producto) => {
+    const {nombre, codigo, descripcion, precio, tipo, img} = producto;
     const cardproducto = document.createElement("div");
     cardproducto.className = 'col-sm-4 mt-5'
     cardproducto.innerHTML = ` 
     <div class="card  bg-dark text-white" style="width: 18rem;">
-        <img class="card-img-top" src="${producto.img}"
-            alt="${producto.nombre}">
+        <img class="card-img-top" src="${img}"
+            alt="${nombre}">
         <div class="card-body">
-            <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text">${producto.descripcion}</p>
-            <p class="card-text">${producto.precio}</p>
-            <button href="#" data-id = "${producto.codigo}" class="botonDeCompra">AGREGAR A CARRITO</button>
+            <h5 class="card-title">${nombre}</h5>
+            <p class="card-text">${descripcion}</p>
+            <p class="card-text">${precio}</p>
+            <button href="#" data-id = "${codigo}" class="botonDeCompra">AGREGAR A CARRITO</button>
         </div>
     </div>`;
     cardcontainer.append(cardproducto);
@@ -24,19 +25,21 @@ arrayProducto.forEach((producto) => {
 const cartContainer = document.querySelector('#cartContainer')
 const botonCompra = document.querySelectorAll('.botonDeCompra')
 const botonCarrito = document.querySelector('#botonCarrito')
-
+const botonEliminar = document.querySelector('.botonEliminar')
 
 const imprimirCarrito = () => {
     cartContainer.innerHTML = ''
     carrito.forEach((producto) => {
+        const {nombre, codigo, descripcion, precio, tipo, img} = producto;
         const cartRow = document.createElement('div')
         cartRow.className = 'cartRow'
         cartRow.innerHTML = `
         <div class="cartImg">
-        <img src="${producto.img}">
+        <img src="${img}">
         </div>
-        <div class="cartTitle"><span> ${producto.nombre}</span></div>
-        <div class="cartPrice"><span> $${producto.precio}</span></div>
+        <div class="cartTitle"><span> ${nombre}</span></div>
+        <div class="cartPrice"><span> $${precio}</span></div>
+        <button class="btn btn-primary botonEliminar" type="submit">X</button>
         `
         cartContainer.append(cartRow)
     })
@@ -45,6 +48,15 @@ const imprimirCarrito = () => {
 carrito.length > 0 && imprimirCarrito() //renderizo el carrito si existe algo dentro
 
 
+
+    
+botonEliminar.forEach((Eliminar) => {
+    Eliminar.addEventListener('click', productoEliminado)
+})
+
+const productoEliminado = (e) => {
+    carrito.slice(0,0) || carrito
+}
 
 const productoAgregado = (e) => {
     const productoElegido = e.target.getAttribute('data-id')
